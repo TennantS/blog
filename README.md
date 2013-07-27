f77e11663da9b1ea9c5f3d6ccff1c51d3dd0bb56
Shane Tennant <shane@trufold.com.au>
2013-07-27 16:42:42 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Things I learned
+* cd = change directory
+* ~ = home directory
+* subl = Open Sublime
+* subl . = Open current directory in sublime
+* rails new blog -d postgresql --skip-test-unit # (New rails project called blog, with postgres db and no testunit)
+
+## Setup pow for new rails app called blog in rails_projects directory
+* cd ~/.pow
+* ln -s /Users/shane/rails_projects/blog
+
+## To install new gems
+* Add new gems to gemfile
+* Run bundle install from the terminal
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'aa88fa3144b1070762494a4a6ed21c2ce252796cb656fd595e4b9d6687fd896ae7370715141f970728654afd1cb69264f7096e6a84cd883c32b8a5b82bccc9d1'
